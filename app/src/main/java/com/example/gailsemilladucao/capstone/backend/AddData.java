f01d2e6b869c087424eb255ef6fdd39be32460e9
gaily <gailchan16@gmail.com>
2018-08-29 01:09:12 +0800

    showdata upload image

--- app/src/main/java/com/example/gailsemilladucao/capstone/backend/AddData.java
+++ app/src/main/java/com/example/gailsemilladucao/capstone/backend/AddData.java
@@ -1,32 +1,40 @@
 package com.example.gailsemilladucao.capstone.backend;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Environment;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.gailsemilladucao.capstone.R;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.io.IOException;
 import java.util.UUID;
 
 public class AddData extends AppCompatActivity {
 
+    ImageView mimage;
     Button recstart,recstop,play,pause;
     String savepath = "";
     MediaRecorder mediaRecorder;
     MediaPlayer mediaPlayer;
 
     final int REQUEST_PERMISSION_CODE = 1000;
+    final int REQUEST_PERMISSION_GALLERY = 999;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +45,8 @@ public class AddData extends AppCompatActivity {
         if(!checkPermissionFromDevice())
             requestPermission();
 
+        //binding
+        mimage = findViewById(R.id.image);
         recstart = findViewById(R.id.recstart);
         recstop =  findViewById(R.id.recstop);
         play = findViewById(R.id.play);
@@ -123,7 +133,22 @@ public class AddData extends AppCompatActivity {
         });
 
 
+        mimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //external storage permission
+                //runtime permision for android 6.0 and above
+                ActivityCompat.requestPermissions(
+                        AddData.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_PERMISSION_GALLERY
+                );
+            }
+        });
+
     }
+
+
 
     private void setupMediaRecorder() {
         mediaRecorder = new MediaRecorder();
@@ -143,6 +168,11 @@ public class AddData extends AppCompatActivity {
     //press CTRL+O
 
 
+
+
+
+
+    //for audio
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode)
@@ -156,6 +186,19 @@ public class AddData extends AppCompatActivity {
                 }
             }
             break;
+
+            case REQUEST_PERMISSION_GALLERY:
+            {
+                if(grantResults.length> 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //gallery intent
+                    Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                    galleryIntent.setType("image/*");
+                    startActivityForResult(galleryIntent, REQUEST_PERMISSION_GALLERY);
+                }else{
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
         }
     }
 
@@ -164,5 +207,31 @@ public class AddData extends AppCompatActivity {
         int record_audio_result = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
         return write_external_storage_result == PackageManager.PERMISSION_GRANTED &&
                 record_audio_result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    //for gallery
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == REQUEST_PERMISSION_GALLERY && resultCode == RESULT_OK) {
+            Uri imageUri = data.getData();
+            CropImage.activity(imageUri)
+                    .setGuidelines(CropImageView.Guidelines.ON) //crop guide lines
+                    .setAspectRatio(1, 1) //square croping
+                    .start(this);
+        }
+
+        if(requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if(resultCode == RESULT_OK){
+                Uri resultUri = result.getUri();
+                //set image choosen from gallery to image view
+                mimage.setImageURI(resultUri);
+            }else if( resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE){
+                Exception error = result.getError();
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
