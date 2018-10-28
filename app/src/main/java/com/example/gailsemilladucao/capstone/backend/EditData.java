c9590c41ec0d330ea8790da441cbdcd906aaf9b7
gaily <gailchan16@gmail.com>
2018-10-28 14:12:48 +0800

    KAreennn

---
+++ app/src/main/java/com/example/gailsemilladucao/capstone/backend/EditData.java
@@ -0,0 +1,15 @@
+package com.example.gailsemilladucao.capstone.backend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.gailsemilladucao.capstone.R;
+
+public class EditData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_data);
+    }
+}
